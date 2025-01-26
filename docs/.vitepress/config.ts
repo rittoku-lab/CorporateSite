07afe50c1e446281932760137745495423e1a15e
satoshi.kato <satoshi.kato@yorozu.io>
2025-01-27 07:11:20 +0900

    set for custom domain

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vitepress";
 
 export default defineConfig({
-  base: "/CorporateSite",
   title: "合同会社リットク",
   description: "Corporate website and content hub",
   lang: "ja-JP",
